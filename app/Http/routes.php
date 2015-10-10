9ce5fe8bca21d5c9e4b7c503308a3ef3db352ac6
Yida Chen <yida820310@gmail.com>
2015-10-10 20:05:05 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,11 +19,11 @@ Route::group(['namespace' => 'front'], function () {
 	Route::post('storeReview', 'reviewController@store');
 });
 
-// 認證路由...
+// 認證路由
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-// 註冊路由...
+// 註冊路由
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
